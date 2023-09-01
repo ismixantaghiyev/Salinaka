84476edf1cc93d4f4fd35ac0dfb8fd295d8bd34a
tagiyevis <116768623+tagiyevis@users.noreply.github.com>
2023-09-01 21:52:14 +0400

    responsive4

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -20,24 +20,24 @@ function Search({ addedToBasket, setAddedToBasket, dataArray, setDataArray, inpu
     }
 
     return (
-        <>        
-        <div className='search'>
-            {filter ? <div onClick={filterClick} className='filter'>
-                <div className='filterText'>Filter</div>
-                <div className='filterIcon'>
-                    <AiOutlineFilter />
+        <>
+            <div className='search'>
+                {filter ? <div onClick={filterClick} className='filter'>
+                    <div className='filterText'>Filter</div>
+                    <div className='filterIcon'>
+                        <AiOutlineFilter />
+                    </div>
+                </div> : null}
+                <div className='inp'>
+                    <input value={value} onChange={inputSearch} type="text" placeholder='Search product' />
+                    <AiOutlineSearch style={{ position: "relative", top: "5px", right: "281", color: "#7A7A7A", fontSize: "20px" }} />
                 </div>
-            </div> : null}
-            <div className='inp'>
-                <input value={value} onChange={inputSearch} type="text" placeholder='Search product' />
-                <AiOutlineSearch style={{ position: "relative", top: "5px", right: "281", color: "#7A7A7A", fontSize: "20px" }} />
+                <div className='basketIconss' onClick={click}>
+                    <GrBasket style={{ fontSize: "25px", cursor: "pointer", position: "relative" }} />
+                    <div className='count' style={{ display: dataArray.length == 0 ? "none" : "block" }}><p>{count}</p></div>
+                </div>
+                <Basket addedToBasket={addedToBasket} setAddedToBasket={setAddedToBasket} dataArray={dataArray} setDataArray={setDataArray} open={open} setOpen={setOpen} />
             </div>
-            <div className='basketIconss' onClick={click}>
-                <GrBasket style={{ fontSize: "25px", cursor: "pointer", position: "relative" }} />
-                <div className='count' style={{ display: dataArray.length == 0 ? "none" : "block" }}><p>{count}</p></div>
-            </div>
-            <Basket addedToBasket={addedToBasket} setAddedToBasket={setAddedToBasket} dataArray={dataArray} setDataArray={setDataArray} open={open} setOpen={setOpen} />
-        </div>
         </>
 
     )
